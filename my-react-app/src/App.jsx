2abbf1fe578d4f38e70231f2a007b4b4941c40ed
Austin Tran <austintran616@gmail.com>
2025-06-21 10:00:45 +0800

    basic set up

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Faq from './pages/Faq'
+import Dashboard from './pages/Dashboard'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
